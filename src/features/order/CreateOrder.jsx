7c5421ae476afcb7cd89960f7d337821573a8683
Oleksandr Polivanov <aldiker@ukr.net>
2024-06-05 16:49:21 +0300

    Editing action in CreateOrder component ...

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import { Form } from 'react-router-dom'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
     /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
         str
-    );
+    )
 
 const fakeCart = [
     {
@@ -28,17 +29,18 @@ const fakeCart = [
         unitPrice: 15,
         totalPrice: 15,
     },
-];
+]
 
-function CreateOrder() {
+export default function CreateOrder() {
     // const [withPriority, setWithPriority] = useState(false);
-    const cart = fakeCart;
+    const cart = fakeCart
 
     return (
         <div>
             <h2>Ready to order? Let's go!</h2>
 
-            <form>
+            {/* <Form method="POST" action='/order/new'> */}
+            <Form method="POST">
                 <div>
                     <label>First Name</label>
                     <input type="text" name="customer" required />
@@ -72,11 +74,29 @@ function CreateOrder() {
                 </div>
 
                 <div>
+                    <input
+                        type="hidden"
+                        name="cart"
+                        value={JSON.stringify(cart)}
+                    />
                     <button>Order now</button>
                 </div>
-            </form>
+            </Form>
         </div>
-    );
+    )
 }
 
-export default CreateOrder;
+export async function action({ request }) {
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData)
+    console.log(data)
+
+    const order = {
+        ...data,
+        priority: data.priority === 'on',
+        cart: JSON.parse(data.cart),
+    }
+    console.log(order)
+
+    return null
+}
