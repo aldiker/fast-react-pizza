68262487a37bc18d6e2883d2bd0f6127bbcd7cde
Oleksandr Polivanov <aldiker@ukr.net>
2024-06-10 20:49:18 +0300

    Styling text in some components

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,12 +1,15 @@
-import { Link } from 'react-router-dom'
-import SearchOrder from '../features/order/SearchOrder'
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 
 export default function Header() {
     return (
-        <header>
-            <Link to="/">Fast React Pizza</Link>
+        <header className="bg-yellow-500 uppercase">
+            <Link to="/" className="tracking-widest">
+                Fast React Pizza Co.
+            </Link>
             <SearchOrder />
-            <p>Oleksandr</p>
+            <UserName />
         </header>
-    )
+    );
 }
