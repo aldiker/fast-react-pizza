912d0af33e0c701e47a55947c2f525feff221c1f
Oleksandr Polivanov <aldiker@ukr.net>
2024-06-12 16:41:23 +0300

    Added Flex and Griid containers and some other Tailwind styles in some components

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,3 +1,5 @@
 export default function UserName() {
-    return <div className="hidden text-sm font-semibold">Oleksandr</div>;
+    return (
+        <div className="hidden text-sm font-semibold md:block">Oleksandr</div>
+    );
 }
