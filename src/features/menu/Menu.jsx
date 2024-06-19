ec311cb07a49e302dabe23227e8ba4ebb197fd2e
Oleksandr Polivanov <aldiker@ukr.net>
2024-06-19 16:54:21 +0300

    Changing Menu and MenuItem components

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,20 +1,20 @@
-import { useLoaderData } from 'react-router-dom'
-import { getMenu } from '../../services/apiRestaurant'
-import MenuItem from './MenuItem'
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 export default function Menu() {
-    const menu = useLoaderData()
+    const menu = useLoaderData();
 
     return (
-        <ul>
+        <ul className="divide-y divide-stone-200">
             {menu.map((pizza) => (
                 <MenuItem pizza={pizza} key={pizza.id} />
             ))}
         </ul>
-    )
+    );
 }
 
 export async function loader() {
-    const menu = await getMenu()
-    return menu
+    const menu = await getMenu();
+    return menu;
 }
