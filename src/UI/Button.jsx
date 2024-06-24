3d55297b55153ac484a484b83fb608af7274c7aa
Oleksandr Polivanov <aldiker@ukr.net>
2024-06-24 16:26:09 +0300

    Added styles to CreateOrder component

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 export default function Button({ children, disabled, to, type }) {
     const base =
-        'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-500 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-stone-300 disabled:cursor-not-allowed disabled:bg-stone-500 disabled:text-stone-400 ';
+        'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-500 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-stone-300 disabled:cursor-not-allowed disabled:bg-stone-500 disabled:text-stone-400 text-sm ';
 
     const styles = {
         primary: base + ' px-4 py-3 md:px-6 md:py-4',
         small: base + ' px-4 py-2 md:px-4 md:py-2 text-xs',
         secondary:
-            'inline-block rounded-full font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-500 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 active:bg-stone-300 disabled:cursor-not-allowed disabled:bg-stone-500 disabled:text-stone-400 border-2 border-stone-300 px-4 py-3 md:px-6 md:py-4',
+            'text-sm inline-block rounded-full font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-500 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 active:bg-stone-300 disabled:cursor-not-allowed disabled:bg-stone-500 disabled:text-stone-400 border-2 border-stone-300 px-4 py-3 md:px-6 md:py-4',
     };
 
     if (to)
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -42,47 +42,53 @@ export default function CreateOrder() {
     const cart = fakeCart;
 
     return (
-        <div>
-            <h2>Ready to order? Let's go!</h2>
+        <div className="px-4 py-6">
+            <h2 className="mb-8 text-xl font-semibold">
+                Ready to order? Let's go!
+            </h2>
 
             {/* <Form method="POST" action='/order/new'> */}
             <Form method="POST">
-                <div>
-                    <label>First Name</label>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">First Name</label>
                     <input
                         type="text"
-                        className="input"
+                        className="input grow"
                         name="customer"
                         required
                     />
                 </div>
 
-                <div>
-                    <label>Phone number</label>
-                    <div>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Phone number</label>
+                    <div className="grow">
                         <input
                             type="tel"
                             name="phone"
                             required
-                            className="input"
+                            className="input w-full"
                         />
+                        {formErrors?.phone && (
+                            <p className="mt-1 rounded-full bg-red-100 p-1 text-center text-xs text-red-700">
+                                {formErrors?.phone}
+                            </p>
+                        )}
                     </div>
-                    {formErrors?.phone && <p>{formErrors?.phone}</p>}
                 </div>
 
-                <div>
-                    <label>Address</label>
-                    <div>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Address</label>
+                    <div className="grow">
                         <input
                             type="text"
                             name="address"
                             required
-                            className="input"
+                            className="input w-full"
                         />
                     </div>
                 </div>
 
-                <div>
+                <div className="mb-12 flex items-center gap-5">
                     <input
                         className="m-4 h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
                         type="checkbox"
@@ -91,7 +97,7 @@ export default function CreateOrder() {
                         // value={withPriority}
                         // onChange={(e) => setWithPriority(e.target.checked)}
                     />
-                    <label htmlFor="priority">
+                    <label htmlFor="priority" className="font-medium">
                         Want to yo give your order priority?
                     </label>
                 </div>
