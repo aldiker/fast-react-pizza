0d967b68006759834a877fe0376c1fae7cf07339
Oleksandr Polivanov <aldiker@ukr.net>
2024-07-03 22:08:28 +0300

    Added useSelector to Cart component. Some settings for VSCode Prettier and ESLint

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
 
 // const initialState = {
 //     cart: [
@@ -13,41 +13,50 @@ import { createSlice } from '@reduxjs/toolkit';
 // };
 const initialState = {
     cart: [],
-};
+}
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
         addItem(state, action) {
-            state.cart.push(action.payload);
+            const newItem = action.payload
+            const existItem = state.cart.find(
+                (item) => item.pizzaId === newItem.pizzaId,
+            )
+            if (existItem) {
+                existItem.quantity++
+                existItem.totalPrice = existItem.quantity * existItem.unitPrice
+            } else {
+                state.cart.push(newItem)
+            }
         },
         deleteItem(state, action) {
             state.cart = state.cart.filter(
                 (item) => item.pizzaId !== action.payload,
-            );
+            )
         },
         increaseItemQuantity(state, action) {
             const item = state.cart.find(
                 (item) => item.pizzaId === action.payload,
-            );
+            )
 
-            item.quantity++;
-            item.totalPrice = item.quantity * item.unitPrice;
+            item.quantity++
+            item.totalPrice = item.quantity * item.unitPrice
         },
         decreaseItemQuantity(state, action) {
             const item = state.cart.find(
                 (item) => item.pizzaId === action.payload,
-            );
+            )
 
-            item.quantity--;
-            item.totalPrice = item.quantity * item.unitPrice;
+            item.quantity--
+            item.totalPrice = item.quantity * item.unitPrice
         },
         clearCart(state, action) {
-            state.cart = [];
+            state.cart = []
         },
     },
-});
+})
 
 export const {
     addItem,
@@ -55,14 +64,16 @@ export const {
     increaseItemQuantity,
     decreaseItemQuantity,
     clearCart,
-} = cartSlice.actions;
+} = cartSlice.actions
 
-export default cartSlice.reducer;
+export default cartSlice.reducer
 
 export const getTotalCartQuantity = (state) =>
-    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0)
 
 export const getTotalCartPrice = (state) =>
-    state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+    state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
+
+export const getCart = (state) => state.cart.cart
 
 // 'reselct'
