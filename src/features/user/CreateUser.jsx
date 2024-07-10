75ea23075c25381341c56b3f416f635439c29c3c
Oleksandr Polivanov <aldiker@ukr.net>
2024-07-10 16:52:58 +0300

    Added fetchAddress to userSlice

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-import Button from '../../UI/Button';
-import { useDispatch } from 'react-redux';
-import { updateName } from './userSlice';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
+import Button from '../../UI/Button'
+import { useDispatch } from 'react-redux'
+import { updateName } from './userSlice'
+import { useNavigate } from 'react-router-dom'
 
 export default function CreateUser() {
-    const [username, setUsername] = useState('');
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+    const [username, setUsername] = useState('')
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     function handleSubmit(e) {
-        e.preventDefault();
-        if (!username) return;
+        e.preventDefault()
+        if (!username) return
 
-        dispatch(updateName(username));
-        navigate('/menu');
+        dispatch(updateName(username))
+        navigate('/menu')
     }
 
     return (
@@ -37,5 +37,5 @@ export default function CreateUser() {
                 </div>
             )}
         </form>
-    );
+    )
 }
